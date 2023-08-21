9ceeb642a1febf2772d3bc48ea20202367ad88b6
Ozgun Munar <ozgunmnr@gmail.com>
2023-08-21 15:12:06 -0400

    change password mail button's text has changed

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -87,7 +87,7 @@ const ProfilePage = () => {
             className="bg-blue-500 hover:bg-blue-700 text-white
             font-bold py-2 px-4 rounded mt-4"
             disabled={isPasswordMailSent}
-            >Change Password</button>
+            >Change Password(Send Change Password Mail)</button>
             
             <Toaster position="top-right" reverseOrder={false}/>
         
