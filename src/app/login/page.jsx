4451c6f00bffbad0018a640078903b6f009b8fc1
Ozgun Munar <ozgunmnr@gmail.com>
2024-11-27 17:51:13 +0300

    some frontend changes

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,14 +3,17 @@
 import SignupPage from "@/app/_components/Authentication/SignUp";
 import LoginPage from "../_components/Authentication/Login";
 import React from 'react'
+import imagepa from '@/app/_images/rustic_flower.png';
 
 export default function Login() {
     
-    const [isSignUp, setIsSignUp] = React.useState(false);
-    
+    const [isSignUp, setIsSignUp] = React.useState(false)
+
     return (
         
         <div className="login_container">
+
+            <img src={imagepa.src} alt="asd" className="absolute left-0 bottom-0 opacity-10 w-full max-w-6xl hidden md:block"/>
 
             <section className='login_text'>
 
