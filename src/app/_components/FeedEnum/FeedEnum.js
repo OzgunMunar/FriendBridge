c561789bdeaaada5f9fa5cca1a440b9d0e2c1b84
Ozgun Munar <ozgunmnr@gmail.com>
2024-12-02 17:56:21 +0300

    one of the feedTypes name changed

--- src/app/_components/FeedEnum/FeedEnum.js
+++ src/app/_components/FeedEnum/FeedEnum.js
@@ -2,7 +2,7 @@ export const feedTypes = Object.freeze({
 
     ProfileFeed: "Only user's posts",
     MainFeed: "Shuffled posts",
-    SavedPostsFeed: "Only user's liked posts",
+    SavedPostsFeed: "Only user's saved posts",
     
     GroupFeed: "Group's posts",
     GlobalFeed: "Admin's posts",
